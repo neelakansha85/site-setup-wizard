4857e54a23a63dbf376127092dfc5f3086346669
Neel Shah <neel@nsdesigners.com>
2016-07-21 20:21:08 -0400

    working on graph for allSiteInfo

--- admin/ssw_analytics_page.php
+++ admin/ssw_analytics_page.php
@@ -1,8 +1,8 @@
 <?php
 
-wp_enqueue_script( 'ssw-analytics-js' );
 wp_enqueue_script( 'd3-min-js' );
 wp_enqueue_script( 'nv-d3-min-js' );
+wp_enqueue_script( 'ssw-analytics-js' );
 
 wp_enqueue_style( 'bootstrap-min-css' );
 wp_enqueue_style( 'nv-d3-min-css' );
@@ -15,23 +15,38 @@ $is_debug_mode = $options['debug_mode'];
 
 $ssw_analytics = new stdClass();
 
+/* Get all sites created using Site Setup Wizard */
+$results = $wpdb->get_results(
+    "SELECT site_type, blog_id, endtime FROM wp_nsd_site_setup_wizard WHERE site_created = 1 AND blog_id <> '' AND blog_id IS NOT NULL"
+    );
+$ssw_analytics->allSitesInfo = $results;
+
 /* Get count of all sites created based on cateogory selected on Step 1 */
 $results = $wpdb->get_results( 
     "SELECT site_type, COUNT(*) as number_of_sites FROM {$ssw_main_table} WHERE site_created = 1 AND site_type <> '' AND site_type IS NOT NULL GROUP BY site_type"
     );
 
-$ssw_analytics->siteTypeResults = $results;
+$ssw_analytics->siteTypeInfo = $results;
 
 wp_localize_script( 'ssw-analytics-js', 'sswAnalytics', $ssw_analytics );
 
 ?>
 <div class="wrap">
     <h1><?php echo esc_html('Site Setup Wizard Analytics') ?></h1>
+    <h4><?php echo esc_html('Number of Sites Created') ?></h3>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <svg id="all-sites-info">
+                </svg>
+            </div>
+        </div>
+    </div>
     <h4><?php echo esc_html('Sites created based on their Type') ?></h3>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
-                <svg id="pie-site-type">
+                <svg id="site-type-info">
                 </svg>
             </div>
         </div>
--- js/ssw-analytics.js
+++ js/ssw-analytics.js
@@ -6,39 +6,202 @@
 	 */
 	$( window ).load(function() {
 
-		loadGraphs();
+//		loadAllSitesInfo();
 
-		function loadGraphs() {
+		loadAllSitesInfo2();
 
-			var height = 400;
-			var width = 400;
+		loadSiteTypeInfo();
 
-			nv.addGraph(function() {
-				var chart = nv.models.pieChart()
-				.x(function(d) { return d.site_type})
-				.y(function(d) { return d.number_of_sites })
-				.showTooltipPercent(true)
-				.width(width)
-				.height(height)
-				.showLabels(true)
-				.labelsOutside(false)
-				.showLegend(true);
+		console.log(sswAnalytics.allSitesInfo);
 
-				d3.select("#pie-site-type")
-				.datum(sswAnalytics.siteTypeResults)
-				.attr('width', width)
-				.attr('height', height)
-				.attr('viewBox', '0 0 ' + width + ' ' + height)
-				.transition().duration(500)
-				.attr('perserveAspectRatio', 'xMinYMid')
-				.call(chart);
-
-				nv.utils.windowResize(chart.update);
-				return chart;
-			});
-		}
 	});
 })( jQuery );
+
+var myData = testData();
+
+var allData = d3.nest()
+  .key(function (d) { return d.site_type; })
+  .entries(sswAnalytics.allSitesInfo)
+  ;
+
+  var type1 = d3.nest()
+  	.key(function (d) { return d.key; })
+  	.sortKeys()
+  	.rollup(function (d) {
+  		return d3.sum(d, function (g) { return 1; });
+  	})
+  	.entries(allData.key)
+  	;
+
+
+
+function loadAllSitesInfo2() {
+	nv.addGraph(function() {
+        var chart = nv.models.lineWithFocusChart();
+        chart.brushExtent([50,70]);
+        chart.xAxis.tickFormat(d3.format(',f')).axisLabel("Stream - 3,128,.1");
+        chart.x2Axis.tickFormat(d3.format(',f'));
+        chart.yTickFormat(d3.format(',.2f'));
+        chart.useInteractiveGuideline(true);
+        d3.select('#all-sites-info')
+            .datum(myData)
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+        return chart;
+    });
+}
+
+function testData() {
+        return stream_layers(3,128,.1).map(function(data, i) {
+            return {
+                key: 'Stream' + i,
+                area: i === 1,
+                values: data
+            };
+        });
+    }
+
+
+/* Inspired by Lee Byron's test data generator. */
+function stream_layers(n, m, o) {
+  if (arguments.length < 3) o = 0;
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < m; i++) {
+      var w = (i / m - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
+  }
+  return d3.range(n).map(function() {
+      var a = [], i;
+      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+      for (i = 0; i < 5; i++) bump(a);
+      return a.map(stream_index);
+    });
+}
+
+/* Another layer generator using gamma distributions. */
+function stream_waves(n, m) {
+  return d3.range(n).map(function(i) {
+    return d3.range(m).map(function(j) {
+        var x = 20 * j / m - i / 3;
+        return 2 * x * Math.exp(-.5 * x);
+      }).map(stream_index);
+    });
+}
+
+function stream_index(d, i) {
+  //return {x: i, y: Math.max(0, d)};
+  return {x: i, y: Math.max(0, d)};
+}
+
+
+/**
+ * Display line chart based on allSitesInfo
+ *
+ * @since 1.5.5
+ */
+function loadAllSitesInfo() {
+
+	var height = 400;
+	var width = 900;
+	var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+
+	nv.addGraph(function() {
+/*
+		var chart = nv.models.line()
+		.x(function(d) { return d.endtime})
+		.y(function(d) { return d })
+		.useInteractiveGuideline(true)
+		.showLegend(true)
+		.showYAxis(true)
+		.showXAxis(true)
+		;
+
+/*    chart.xAxis     //Chart x-axis settings
+      .axisLabel('Time')
+      .tickFormat(parseDate);
+
+  	chart.yAxis     //Chart y-axis settings
+      .axisLabel('Number of Sites')
+      .tickFormat(d3.format(',r'));
+*/
+
+			var chart = nv.models.line()
+                .width(width)
+                .height(height)
+                .margin({top: 20, right: 20, bottom: 20, left: 20});
+
+
+			chart.xAxis     //Chart x-axis settings
+      .axisLabel('Time (ms)')
+      .tickFormat(d3.format(',r'));
+
+  		chart.yAxis     //Chart y-axis settings
+      .axisLabel('Voltage (v)')
+      .tickFormat(d3.format('.02f'));
+
+      
+console.log(myData);
+		d3.select("#all-sites-info")
+//		.datum(sswAnalytics.allSitesInfo)
+		.datum(myData)
+		.attr('width', width)
+		.attr('height', height)
+		.call(chart);
+
+		nv.utils.windowResize(chart.update);
+		return chart;
+	});
+
+
+}
+
+
+
+/**
+ * Display pie chart based on siteTypeInfo
+ *
+ * @since 1.5.5
+ */
+function loadSiteTypeInfo() {
+
+	var height = 400;
+	var width = 400;
+
+	nv.addGraph(function() {
+		var chart = nv.models.pieChart()
+		.x(function(d) { return d.site_type})
+		.y(function(d) { return d.number_of_sites })
+		.showTooltipPercent(true)
+		.width(width)
+		.height(height)
+		.showLabels(true)
+		.labelsOutside(false)
+		.labelType(function(d, i, values) {
+			return values.key + ':' + values.value;
+		})
+		.showLegend(true)
+		;
+
+		d3.select("#site-type-info")
+		.datum(sswAnalytics.siteTypeInfo)
+		.attr('width', width)
+		.attr('height', height)
+		.attr('viewBox', '0 0 ' + width + ' ' + height)
+		.transition().duration(500)
+		.attr('perserveAspectRatio', 'xMinYMid')
+		.call(chart)
+		;
+
+		nv.utils.windowResize(chart.update);
+		return chart;
+	});
+}
+
+
 
 /**
  * Bootstrap Hack For D3 graphs
